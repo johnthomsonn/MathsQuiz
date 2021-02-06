5bd182a77bd8dcb080913b236b2a878c2711ae8f
johnthomson16@hotmail.co.uk <johnthomson16@hotmail.co.uk>
2021-02-06 16:09:30 +0000

    handle input number of questions better

--- Methods.py
+++ Methods.py
@@ -1,10 +1,15 @@
 import sys
 
-def getTotalQuestions():    
+def getTotalQuestions(): 
     try:
         questions = int(sys.argv[1])
-    except:
+        print("You are playing " + str(questions) + " questions.")
+    except ValueError:
         questions = 10
+        print("Invalid input. Playing for 10 questions.")
+    except:
+        questions = 5
+        print("You are playing 5 quesions.")
     return questions
 
 def getTypes():
