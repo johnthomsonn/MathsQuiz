277e4ce6120d3b8248f99bb9863830772babb5a5
johnthomsonn <johnthomson16@hotmail.co.uk>
2021-02-09 17:43:52 +0000

    Question class now capable of / and *

--- Question.py
+++ Question.py
@@ -3,11 +3,17 @@ import random, Methods
 class Question:
 
     def __init__(self,maxNumber=20):
-        self.left = random.randint(1,maxNumber)
-        self.right = random.randrange(0, self.left)
-        self.correct = None
-        self.userAnswer = None
         self.type = self.determineType()
+        if self.type == "/":
+            self.createDivisionQuiestion()
+        elif self.type == "*":
+            self.left = random.randint(1,10)
+            self.right = random.randint(1,10)
+        else:
+            self.left = random.randint(1,maxNumber)
+            self.right = random.randint(0, self.left)
+        self.correct = None
+        self.userAnswer = None    
 
 
     def getAnswer(self):
@@ -15,6 +21,10 @@ class Question:
             return self.left + self.right
         elif self.type == "-":
             return self.left - self.right
+        elif self.type == "*":
+            return self.left * self.right
+        elif self.type == "/":
+            return self.left / self.right
 
     def __str__(self):
         return str(self.left) + self.type + str(self.right) + " = "
@@ -28,5 +38,13 @@ class Question:
         return str(self.left) + self.type + str(self.right) + " = " + str(self.userAnswer) + " is " + str(self.correct)
 
     def determineType(self):
-        return Methods.getTypes()[random.randint(0,1)]
+        return Methods.getTypes()[random.randint(3,3)]
+    
+    def createDivisionQuiestion(self):
+        self.right = random.randint(1,10)
+        tmp = random.randint(1,10)
+        self.left = self.right * tmp
+        
+
+    
             
